8bcd07e2d4f05303fd210b79f3aad026eca945cd
Viktor Pasynok <v.pasynok@space307.com>
2021-12-05 00:34:19 +0300

    maintain: reduce devdeps

--- src/__tests__/index.spec.tsx
+++ src/__tests__/index.spec.tsx
@@ -1,7 +1,5 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 
 import {
   SLOTS,
@@ -27,13 +25,13 @@ describe('Test rendering', () => {
       api.set({ id: SLOTS.MAIN, component: Heading });
     });
 
-    expect(screen.getByRole('heading')).toHaveTextContent(HEADING_TEXT);
+    expect(screen.getByRole('heading').textContent).toBe(HEADING_TEXT);
 
     act(() => {
       api.set({ id: SLOTS.MAIN, component: Button });
     });
 
-    expect(screen.getByRole('button')).toHaveTextContent(BUTTON_TEXT);
+    expect(screen.getByRole('button').textContent).toBe(BUTTON_TEXT);
 
     act(() => {
       api.remove({ id: SLOTS.MAIN });
@@ -50,22 +48,23 @@ describe('Test rendering', () => {
       api.set({ id: SLOTS.AWESOME, component: Button });
     });
 
-    expect(screen.getByRole('button')).toHaveTextContent(BUTTON_TEXT);
-    expect(screen.getByRole('heading')).toHaveTextContent(HEADING_TEXT);
+    expect(screen.getByRole('button').textContent).toBe(BUTTON_TEXT);
+    expect(screen.getByRole('heading').textContent).toBe(HEADING_TEXT);
 
     act(() => {
       api.remove({ id: SLOTS.MAIN });
     });
 
-    expect(screen.getByRole('button')).toHaveTextContent(BUTTON_TEXT);
+    expect(screen.getByRole('button').textContent).toBe(BUTTON_TEXT);
     expect(container.firstChild?.nodeName).toBe('BUTTON');
-    expect(screen.queryByText(HEADING_TEXT)).not.toBeInTheDocument();
+
+    expect(screen.queryByText(HEADING_TEXT)).toBe(null);
 
     act(() => {
       api.remove({ id: SLOTS.AWESOME });
     });
 
-    expect(screen.queryByText(BUTTON_TEXT)).not.toBeInTheDocument();
+    expect(screen.queryByText(BUTTON_TEXT)).toBe(null);
   });
 
   test('Slots render [hide and show]', () => {
@@ -76,47 +75,47 @@ describe('Test rendering', () => {
       api.set({ id: SLOTS.AWESOME, component: Button });
     });
 
-    expect(screen.getByRole('heading')).toHaveTextContent(HEADING_TEXT);
-    expect(screen.getByRole('button')).toHaveTextContent(BUTTON_TEXT);
+    expect(screen.getByRole('heading').textContent).toBe(HEADING_TEXT);
+    expect(screen.getByRole('button').textContent).toBe(BUTTON_TEXT);
 
     act(() => {
       api.hide({ id: SLOTS.MAIN });
     });
 
-    expect(screen.queryByText(HEADING_TEXT)).not.toBeInTheDocument();
-    expect(screen.getByRole('button')).toHaveTextContent(BUTTON_TEXT);
+    expect(screen.queryByText(HEADING_TEXT)).toBe(null);
+    expect(screen.getByRole('button').textContent).toBe(BUTTON_TEXT);
 
     act(() => {
       api.hide({ id: SLOTS.AWESOME });
     });
 
-    expect(screen.queryByText(BUTTON_TEXT)).not.toBeInTheDocument();
+    expect(screen.queryByText(BUTTON_TEXT)).toBe(null);
 
     act(() => {
       api.show({ id: SLOTS.MAIN });
     });
 
-    expect(screen.getByRole('heading')).toHaveTextContent(HEADING_TEXT);
-    expect(screen.queryByText(BUTTON_TEXT)).not.toBeInTheDocument();
+    expect(screen.getByRole('heading').textContent).toBe(HEADING_TEXT);
+    expect(screen.queryByText(BUTTON_TEXT)).toBe(null);
 
     act(() => {
       api.show({ id: SLOTS.AWESOME });
     });
 
-    expect(screen.getByRole('heading')).toHaveTextContent(HEADING_TEXT);
-    expect(screen.getByRole('button')).toHaveTextContent(BUTTON_TEXT);
+    expect(screen.getByRole('heading').textContent).toBe(HEADING_TEXT);
+    expect(screen.getByRole('button').textContent).toBe(BUTTON_TEXT);
   });
 
   test('Slot with fallback', () => {
     let { container } = render(<ComponentWithFallbackSlot />);
 
-    expect(screen.queryByText(FALLBACK_TEXT)).toBeInTheDocument();
+    expect(screen.queryByText(FALLBACK_TEXT)?.textContent).toBe(FALLBACK_TEXT);
 
     act(() => {
       api.set({ id: SLOTS.WITH_FALLBACK, component: Div });
     });
 
-    expect(screen.queryByText(NOT_FALLBACK_TEXT)).toBeInTheDocument();
+    expect(screen.queryByText(NOT_FALLBACK_TEXT)?.textContent).toBe(NOT_FALLBACK_TEXT);
 
     act(() => {
       api.set({ id: SLOTS.WITH_FALLBACK, component: () => null });
@@ -128,18 +127,18 @@ describe('Test rendering', () => {
       api.remove({ id: SLOTS.WITH_FALLBACK });
     });
 
-    expect(screen.queryByText(FALLBACK_TEXT)).toBeInTheDocument();
+    expect(screen.queryByText(FALLBACK_TEXT)?.textContent).toBe(FALLBACK_TEXT);
 
     act(() => {
       api.hide({ id: SLOTS.WITH_FALLBACK });
     });
 
-    expect(screen.queryByText(FALLBACK_TEXT)).not.toBeInTheDocument();
+    expect(screen.queryByText(FALLBACK_TEXT)).toBe(null);
 
     act(() => {
       api.show({ id: SLOTS.WITH_FALLBACK });
     });
 
-    expect(screen.queryByText(FALLBACK_TEXT)).toBeInTheDocument();
+    expect(screen.queryByText(FALLBACK_TEXT)?.textContent).toBe(FALLBACK_TEXT);
   });
 });
